ac80bb4f9bb6a0e364a5c505a879a336185d5976
selimyasar <selimyasar772@gmail.com>
2023-04-25 16:27:30 +0200

    act14

--- app/controllers/wood.js
+++ app/controllers/wood.js
@@ -3,7 +3,7 @@ const { Wood } = require('../models/wood.js');
 exports.getAllWoods = async (req, res) => {
   try {
     const woods = await Wood.findAll();
-    res.json(woods);
+    res.status(200).json(woods);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Une erreur est survenue lors de la récupération des types de bois.' });
