8b84f3e6bfb0554c23e56d14278f54251b406dca
selimyasar <selimyasar772@gmail.com>
2023-04-25 13:47:50 +0200

    act12finale

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -8,10 +8,8 @@
 const { User } = require('../models/user.js');
 
 exports.signup = async (req, res) => {
-  const { firstName, lastName, email, password } = req.body;
-
   try {
-    const user = await User.create({ firstName, lastName, email, password });
+    const user = await User.create({ ...req.body });
 
     res.status(201).json(user);
   } catch (error) {
@@ -19,7 +17,6 @@ exports.signup = async (req, res) => {
     res.status(500).json({ message: 'Une erreur est survenue lors de la création du compte utilisateur.' });
   }
 };
-
 
 exports.login = (req, res) => {
   res.send("You are logged in");
