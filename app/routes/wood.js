f9ec1c5753fecd31f38a67fab0c8e0309a6ddc56
selimyasar <selimyasar772@gmail.com>
2023-04-26 15:50:05 +0200

    maj

--- app/routes/wood.js
+++ app/routes/wood.js
@@ -12,6 +12,9 @@ const router = express();
 const userCtrl = require("../controllers/wood.js");
 const woodController = require('../controllers/wood');
 const auth = require("../middleware/auth.js")
+router.post('/signup', function (req, res) {
+    res.send('You are signup');
+    });
 
 router.get("/", auth, woodCtrl.readAll);
 router.get("/:hardness", auth, woodCtrl.readByHardness);
