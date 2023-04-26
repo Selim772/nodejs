2333c5dd3d00be4e8f8cd626be35c877dba5a6cb
selimyasar <selimyasar772@gmail.com>
2023-04-26 11:24:38 +0200

    act16

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,20 +1,28 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const { User } = require('../models/user.js');
 
-exports.signup = async (req, res) => {
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
 
   try {
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.findOne({ where: { email } });
 
-    const user = await User.create({ ...req.body, password: hashedPassword });
+    if (!user) {
+      return res.status(401).json({ message: "L'adresse e-mail ou le mot de passe est incorrect." });
+    }
 
-    res.status(201).json(user);
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ message: "L'adresse e-mail ou le mot de passe est incorrect." });
+    }
+
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '24h' });
+
+    res.status(200).json({ message: "Vous êtes connecté.", token });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Une erreur est survenue lors de la création du compte utilisateur.' });
+    res.status(500).json({ message: "Une erreur est survenue lors de l'authentification de l'utilisateur." });
   }
-};
-
-exports.login = (req, res) => {
-  res.send("You are logged in");
 };
