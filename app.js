b10f7b25b5f8d0564bffef53783ac41d780ef119
selimyasar <selimyasar772@gmail.com>
2023-04-27 15:03:08 +0200

    act19

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require("express");
-var path = require('path');
+const cors = require("cors");
+const path = require("path");
+
 const app = express();
-const router = require("./app/routes/index.js");
+
+const corsOptions = {
+  origin: "http://localhost:8080",
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
-//Ajout des routes
-app.use("/api", router);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+const router = require("./app/routes/index.js");
+app.use("/api", router);
 
 const db = require("./app/models/index.js");
 db.sequelize
   .authenticate()
   .then(() => console.log("Database connected ..."))
   .catch((err) => console.log(err));
+
 module.exports = app;
