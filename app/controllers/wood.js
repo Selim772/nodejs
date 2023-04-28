c43ed70dcc28170c3ba3ff6df2fd1973ebc45a3c
selimyasar <selimyasar772@gmail.com>
2023-04-28 13:06:34 +0200

    update delete

--- app/controllers/wood.js
+++ app/controllers/wood.js
@@ -26,7 +26,6 @@ exports.readByHardness = async (req, res) => {
 exports.createWood = async (req, res) => {
   const { filename } = req.file;
   const pathname = `${req.protocol}://${req.get("host")}/uploads/${filename}`;
-  const { name, hardness, ...rest } = JSON.parse(req.body.datas);
 
   try {
     const wood = await Wood.create({ ...JSON.parse(req.body.datas), image: pathname });
@@ -41,3 +40,45 @@ exports.createWood = async (req, res) => {
     res.status(500).json({ message: 'Une erreur est survenue lors de la création de l\'essence de bois.' });
   }
 };
+
+exports.updateWood = async (req, res) => {
+    const { id } = req.params;
+    const { filename } = req.file;
+    const pathname = `${req.protocol}://${req.get("host")}/uploads/${filename}`;
+    const updatedWood = await Wood.update({ ...JSON.parse(req.body.datas), image: pathname });
+  
+    try {
+      let wood = await Wood.findByPk(id);
+      if (!wood) {
+        res.status(404).json({ message: 'Non trouvée' });
+        return;
+      }
+  
+      res.status(200).json(updatedWood);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Une erreur est survenue lors de la mise à jour de l\'essence de bois.' });
+    }
+  };
+
+  exports.deleteWood = async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+      const deletedWood = await Wood.destroy({ where: { id } });
+  
+      if (deletedWood === 0) {
+        return res.status(404).json({ message: `L'essence de bois ${id} n'existe pas.` });
+      }
+  
+      res.status(200).json({ message: `L'essence de bois ${id} a été supprimée.` });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Une erreur est survenue lors de la suppression de l\'essence de bois.' });
+    }
+  };
+  
+  
+  
+
+
